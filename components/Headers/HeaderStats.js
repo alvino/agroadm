2dc206ac9df312afc7c50a0984c511d39093ee0b
alvino <crazyduckalvino@gmail.com>
2021-04-03 19:27:36 -0300

    alteracao para utilizacao do cardstart

--- components/Headers/HeaderStats.js
+++ components/Headers/HeaderStats.js
@@ -1,10 +1,37 @@
 import React from "react";
 
+import fb from "../../server/firebase";
 // components
 
 import CardStats from "components/Cards/CardStats.js";
+import CardStatsPasto from "components/Cards/CardStatsPasto";
 
 export default function HeaderStats() {
+  const storageFazenda = JSON.parse(sessionStorage.getItem("fazenda"));
+
+  const [pastos, setPastos] = React.useState([]);
+
+  React.useEffect(() => {
+    const init = async () => {
+      const db = fb.firestore();
+      const fazendaRef = db.collection("fazenda").doc(storageFazenda.id);
+      const pastoRef = fazendaRef.collection("pasto");
+
+      const snapshot = await pastoRef.get();
+
+      const data = snapshot.docs.map((item) => ({
+        id: item.id,
+        path: item.ref.path,
+        ...item.data(),
+      }));
+
+      setPastos(data);
+      console.log(data);
+    };
+
+    init();
+  }, []);
+
   return (
     <>
       {/* Header */}
@@ -15,29 +42,32 @@ export default function HeaderStats() {
             <div className="flex flex-wrap">
               <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
                 <CardStats
-                  statSubtitle="TRAFFIC"
-                  statTitle="350,897"
-                  statArrow="up"
-                  statPercent="3.48"
-                  statPercentColor="text-green-500"
-                  statDescripiron="Since last month"
-                  statIconName="far fa-chart-bar"
-                  statIconColor="bg-red-500"
+                  statSubtitle="PASTO"
+                  statTitle={pastos.length}
+                  statArrow={null}
+                  statPercent={null}
+                  statPercentColor={null}
+                  statDescripiron="Pastos cadastrados"
+                  statIconName={null}
+                  statIconColor={null}
                 />
               </div>
               <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
                 <CardStats
-                  statSubtitle="NEW USERS"
-                  statTitle="2,356"
-                  statArrow="down"
-                  statPercent="3.48"
-                  statPercentColor="text-red-500"
-                  statDescripiron="Since last week"
-                  statIconName="fas fa-chart-pie"
-                  statIconColor="bg-orange-500"
+                  statSubtitle="Area (ha)"
+                  statTitle={pastos.reduce(
+                    (acc, item) => (acc += parseFloat(item.area)),
+                    0
+                  )}
+                  statArrow={null}
+                  statPercent={null}
+                  statPercentColor={null}
+                  statDescripiron="Total de área"
+                  statIconName={null}
+                  statIconColor={null}
                 />
               </div>
-              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
+              {/* <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
                 <CardStats
                   statSubtitle="SALES"
                   statTitle="924"
@@ -60,7 +90,7 @@ export default function HeaderStats() {
                   statIconName="fas fa-percent"
                   statIconColor="bg-blue-500"
                 />
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
