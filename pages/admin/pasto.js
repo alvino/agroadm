e56428e4be9c9a5230f2d0c68cf491c6b4a44f0e
alvino <crazyduckalvino@gmail.com>
2021-04-03 19:28:41 -0300

    criacao do modulo pasto

---
+++ pages/admin/pasto.js
@@ -0,0 +1,20 @@
+import React from "react";
+// components
+import CardPastoSettings from "components/Cards/CardPastoSettings";
+
+// layout for page
+import Admin from "layouts/Admin.js";
+
+export default function Fazenda() {
+  return (
+    <>
+      <div className="flex flex-wrap">
+        <div className="w-full  px-4">
+          <CardPastoSettings />
+        </div>
+      </div>
+    </>
+  );
+}
+
+Fazenda.layout = Admin;
