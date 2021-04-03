42a8c130891c35f6c43e599aa8557272e852bc9b
alvino <crazyduckalvino@gmail.com>
2021-04-03 19:36:33 -0300

    ateracao

--- pages/admin/tables.js
+++ pages/admin/tables.js
@@ -11,7 +11,7 @@ import Admin from "layouts/Admin.js";
 export default function Tables() {
   return (
     <>
-      <div className="flex flex-wrap mt-4">
+      <div className="flex flex-wrap">
         <div className="w-full mb-12 px-4">
           <CardTable />
         </div>
