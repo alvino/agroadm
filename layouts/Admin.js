8c1c9aeae2510bbbe69fcaf93bdb51425535bf47
alvino <crazyduckalvino@gmail.com>
2021-04-03 19:32:50 -0300

    apresentando navbar dashboard

--- layouts/Admin.js
+++ layouts/Admin.js
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 
 // components
 
-// import AdminNavbar from "components/Navbars/AdminNavbar.js";
+import AdminNavbar from "components/Navbars/AdminNavbar.js";
 import Sidebar from "components/Sidebar/Sidebar.js";
 import HeaderStats from "components/Headers/HeaderStats.js";
 import HeaderDefault from "components/Headers/HeaderDefault.js";
@@ -17,11 +17,11 @@ export default function Admin({ children }) {
     <>
       <Sidebar />
       <div className="relative md:ml-64 bg-gray-200">
-        {/*<AdminNavbar />*/}
+        <AdminNavbar />
         {/* Header */}
         {router.pathname.indexOf("/admin/dashboard") ? <HeaderDefault /> : <HeaderStats />}
         
-        <div className="px-4 md:px-10 mx-auto w-full -m-24">
+        <div className="px-4 md:px-10 mx-auto w-full -m-16">
           {children}
           <FooterAdmin />
         </div>
