4770fa36bccfdec230f6d882be9946ef9efba5b6
alvino <crazyduckalvino@gmail.com>
2021-04-28 18:15:24 -0300

    api fazanda desaclopando os metodos

--- pages/api/fazenda.js
+++ pages/api/fazenda.js
@@ -1,42 +1,43 @@
 import fb from "../../server/firebase";
 import firebase from "firebase";
 
+const db = fb.firestore();
+
+const get = async (req, res) => {
+  const snapshot = await db.collection("fazenda").get();
+
+  const data = await snapshot.docs.map((item) => ({
+    id: item.id,
+    path: item.ref.path,
+    ...item.data(),
+  }));
+
+  res.status(200).json(data);
+};
+
+const post = async (req, res) => {
+  const { body } = req;
+  const data = {
+    ...body,
+    marker: new firebase.firestore.GeoPoint(
+      body.marker.latitude,
+      body.marker.longitude
+    ),
+  };
+
+  const resposta = await db.collection("fazenda").doc().set(data);
+  res.status(200).json(resposta);
+};
+
 export default async function useHandler(req, res) {
-  const db = fb.firestore();
-
-  const getFazenda = async () => {
-    const snapshot = await db.collection("fazenda").get();
-
-    const data = await snapshot.docs.map((item) => ({
-      id: item.id,
-      path: item.ref.path,
-      ...item.data(),
-    }));
-
-    return data;
-  };
-
-  const postFazenda = async (data) => {
-    const fazenda = {
-      ...data,
-      marker: new firebase.firestore.GeoPoint(
-        data.marker.latitude,
-        data.marker.longitude
-      ),
-    };
-
-    const resposta = await db.collection("fazenda").doc().set(fazenda);
-    return resposta;
-  };
-
   const { method } = req;
 
   switch (method) {
     case "GET":
-      res.status(200).json(await getFazenda());
+      await get(req, res);
       break;
     case "POST":
-      res.status(200).json(postFazenda(req.body));
+      await post(req, res);
       break;
     default:
       res.setHeader("Allow", ["GET", "POST"]);
