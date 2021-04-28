7d0dced992bf81aa2d026282774a57a5b7ea8d80
alvino <crazyduckalvino@gmail.com>
2021-04-28 20:13:15 -0300

    criando um function arrow para useSWR

--- server/axios.js
+++ server/axios.js
@@ -4,4 +4,6 @@ const instance = axios.create({
   baseURL: "http://localhost:3000/api/",
 });
 
+export const fetcher = (url) => instance.get(url).then((res) => res.data);
+
 export default instance;
