600f636003446527bfc3b38abdd60de67fd397f9
alvino <crazyduckalvino@gmail.com>
2021-04-28 17:41:43 -0300

    adicionando useSWR

--- components/Dropdowns/FazendaDropdown.js
+++ components/Dropdowns/FazendaDropdown.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Link from "next/link";
+import useSWR from "swr";
+
 import { createPopper } from "@popperjs/core";
 
 import axios from "../../server/axios";
+
+const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 const LinkDropdown = (props) => {
   const handleOnClick = () => {
@@ -32,6 +36,8 @@ const FazendaDropdown = () => {
   const btnDropdownRef = React.createRef();
   const popoverDropdownRef = React.createRef();
 
+  const { data } = useSWR("fazenda", fetcher);
+
   const openDropdownPopover = () => {
     createPopper(btnDropdownRef.current, popoverDropdownRef.current, {
       placement: "bottom-start",
@@ -41,17 +47,6 @@ const FazendaDropdown = () => {
   const closeDropdownPopover = () => {
     setDropdownPopoverShow(false);
   };
-
-  const [fazendas, setFazendas] = React.useState([]);
-
-  React.useEffect(() => {
-    async function init() {
-      const resp = await axios.get("fazenda");
-      setFazendas(resp.data);
-    }
-
-    init();
-  }, []);
 
   return (
     <>
@@ -73,74 +68,13 @@ const FazendaDropdown = () => {
           "bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
         }
       >
-        {fazendas.map((item, index) => {
+        {data.map((item, index) => {
           return (
             <LinkDropdown key={index} href={`/admin/dashboard`} data={item}>
               {item.descricao}
             </LinkDropdown>
           );
         })}
-
-        {/* <LinkDropdown href="/admin/dashboard">Dashboard</LinkDropdown>
-        <LinkDropdown href="/admin/settings">Settings</LinkDropdown>
-        <LinkDropdown href="/admin/tables">Tables</LinkDropdown>
-        <LinkDropdown href="/admin/maps">Maps</LinkDropdown>
-        <div className="h-0 mx-4 my-2 border border-solid border-gray-200" />
-        <span
-          className={
-            "text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-no-wrap bg-transparent text-gray-500"
-          }
-        >
-          Auth Layout
-        </span>
-        <Link href="/auth/login">
-          <a
-            href="#pablo"
-            className={
-              "text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
-            }
-          >
-            Login
-          </a>
-        </Link>
-        <Link href="/auth/register">
-          <a
-            href="#pablo"
-            className={
-              "text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
-            }
-          >
-            Register
-          </a>
-        </Link>
-        <div className="h-0 mx-4 my-2 border border-solid border-gray-200" />
-        <span
-          className={
-            "text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-no-wrap bg-transparent text-gray-500"
-          }
-        >
-          No Layout
-        </span>
-        <Link href="/landing">
-          <a
-            href="#pablo"
-            className={
-              "text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
-            }
-          >
-            Lading
-          </a>
-        </Link>
-        <Link href="/profile">
-          <a
-            href="#pablo"
-            className={
-              "text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
-            }
-          >
-            Profile
-          </a>
-        </Link> */}
       </div>
     </>
   );
