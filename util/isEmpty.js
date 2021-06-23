f995fe40094882ccdfefb7862ddd7cd74875708c
alvino <crazyduckalvino@gmail.com>
2021-06-22 21:43:11 -0300

    removendo arquivos e dependencias

--- util/isEmpty.js
+++
@@ -1,8 +0,0 @@
-export default function isEmpty(obj) {
-  for (var prop in obj) {
-    if (obj.hasOwnProperty(prop)) {
-      return false;
-    }
-  }
-  return JSON.stringify(obj) === JSON.stringify({});
-}
